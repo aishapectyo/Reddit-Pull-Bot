ab05482ba0179b34759af9a021f0f761912dd4a4
Aisha Pectyo <aisha.mahmoud@gmail.com>
2017-08-08 19:37:49 -0500

    Create simplepull_bot.py

---
+++ simplepull_bot.py
@@ -0,0 +1,32 @@
+""" Code to do a basic Reddit pull using the hottest topics on a subreddit.  Simple bot to track posts
+Aisha Pectyo, 8/8/2017"""
+#Documentation: http://praw.readthedocs.io/en/latest/tutorials/reply_bot.html
+
+#---Import Libraries---#
+import praw
+from sklearn.datasets import fetch_20newsgroups
+
+#---Credentials---#
+#Credentials hidden.  Need to use your personal credentials
+reddit = praw.Reddit(client_id='', client_secret='',user_agent='', username = '', password = '')
+print(reddit.user.me()) #make sure your credentials work
+
+#---Loop through subreddit---#
+titles = [] #placeholder for posts' titles
+urls = [] #placeholder for urls
+for submission in reddit.subreddit('science').hot(limit=1000):
+    titles.append(submission.title)
+    urls.append(submission.url)
+    
+#---Create bot---#
+"""All you need to do is make use of the stream object to indefinitely iterate over new submissions to a subreddit."""
+subreddit = reddit.subreddit('science')
+sub= subreddit.stream.submission()
+
+#---Run loop to get info from bot---#
+titles_bot = []
+urls_bot = []
+for submission in sub:
+  titles_bot.append(submission.title)
+        
+#---Save to CSV file----#
